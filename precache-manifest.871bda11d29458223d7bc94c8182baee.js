6608e8ec5979c7ac4d7764635e854977ed62d300
José Antonio <jos.valga@gmail.com>
2021-04-23 18:29:37 -0500

    Updates

---
+++ precache-manifest.871bda11d29458223d7bc94c8182baee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9d0d052973f17611649db5acc48d6f",
+    "url": "/gallery-app/index.html"
+  },
+  {
+    "revision": "43228e745de6b8842295",
+    "url": "/gallery-app/static/css/main.ffd809f5.chunk.css"
+  },
+  {
+    "revision": "6f9437942bbdd44ded70",
+    "url": "/gallery-app/static/js/2.f8ea4ba1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gallery-app/static/js/2.f8ea4ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43228e745de6b8842295",
+    "url": "/gallery-app/static/js/main.35634158.chunk.js"
+  },
+  {
+    "revision": "d12fd6e5626969580626",
+    "url": "/gallery-app/static/js/runtime-main.9059cfa6.js"
+  }
+]);
